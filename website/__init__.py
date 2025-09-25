491ee3f376f302fb93cc492860981f5f6c4c41e4
yorozc <yorozcolop49@gmail.com>
2025-09-24 21:14:10 -0700

    changes

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ Scss(app)
 
 # protects from cross-site scripting
 
-app.config["SECRET_KEY"] = "4bb18d83f092ecdbfcb9a027b7ca4077e744c79e"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-key")
 
 from .routes import routes
 from .auth import auth
